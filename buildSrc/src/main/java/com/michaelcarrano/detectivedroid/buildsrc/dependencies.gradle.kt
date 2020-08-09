bc1a131a5ce6343cbb198ef00f9e632926da23e7
Michael Carrano <carrano.michael@gmail.com>
2020-08-09 17:45:29 -0400

    Update dependencies

--- buildSrc/src/main/java/com/michaelcarrano/detectivedroid/buildsrc/dependencies.gradle.kt
+++ buildSrc/src/main/java/com/michaelcarrano/detectivedroid/buildsrc/dependencies.gradle.kt
@@ -5,17 +5,17 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha02"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha07"
     const val playPublisherPlugin = "com.github.triplet.gradle:play-publisher:2.6.1"
     const val jacoco = "org.jacoco:org.jacoco.core:0.8.5"
 
-    const val desugar = "com.android.tools:desugar_jdk_libs:1.0.5"
+    const val desugar = "com.android.tools:desugar_jdk_libs:1.0.9"
 
     const val timber = "com.jakewharton.timber:timber:4.7.1"
 
     const val json = "org.json:json:20180813"
     const val junit = "junit:junit:4.13"
-    const val robolectric = "org.robolectric:robolectric:4.3.1"
+    const val robolectric = "org.robolectric:robolectric:4.4-alpha-2"
     const val mockK = "io.mockk:mockk:1.10.0"
     const val mockitoKtx = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
 
@@ -25,7 +25,7 @@ object Libs {
     const val roxie = "com.ww:roxie:0.4.0"
 
     object Google {
-        const val material = "com.google.android.material:material:1.3.0-alpha01"
+        const val material = "com.google.android.material:material:1.3.0-alpha02"
         const val openSourceLicensesPlugin = "com.google.android.gms:oss-licenses-plugin:0.10.2"
         const val openSourceLicensesLibrary =
             "com.google.android.gms:play-services-oss-licenses:17.0.0"
@@ -49,12 +49,12 @@ object Libs {
 
     object AndroidX {
         const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha01"
-        const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta6"
+        const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-rc1"
         const val preference = "androidx.preference:preference:1.1.1"
-        const val recyclerview = "androidx.recyclerview:recyclerview:1.2.0-alpha03"
+        const val recyclerview = "androidx.recyclerview:recyclerview:1.2.0-alpha05"
 
         object Navigation {
-            private const val version = "2.3.0-rc01"
+            private const val version = "2.3.0"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val ui = "androidx.navigation:navigation-ui-ktx:$version"
             const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:$version"
@@ -67,15 +67,15 @@ object Libs {
         }
 
         object Test {
-            private const val version = "1.3.0-rc01"
+            private const val version = "1.3.0-rc03"
             const val core = "androidx.test:core:$version"
             const val runner = "androidx.test:runner:$version"
             const val rules = "androidx.test:rules:$version"
 
-            const val junit = "androidx.test.ext:junit:1.1.2-rc01"
+            const val junit = "androidx.test.ext:junit:1.1.2-rc03"
             const val archCoreTesting = "androidx.arch.core:core-testing:2.1.0"
 
-            const val espressoCore = "androidx.test.espresso:espresso-core:3.3.0-rc01"
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.3.0-rc03"
         }
 
         object Paging {
@@ -107,7 +107,7 @@ object Libs {
     }
 
     object Dagger {
-        private const val version = "2.28"
+        private const val version = "2.28.3"
         const val dagger = "com.google.dagger:dagger:$version"
         const val androidSupport = "com.google.dagger:dagger-android-support:$version"
         const val compiler = "com.google.dagger:dagger-compiler:$version"
@@ -115,7 +115,7 @@ object Libs {
     }
 
     object Facebook {
-        const val flipper = "com.facebook.flipper:flipper:0.46.0"
+        const val flipper = "com.facebook.flipper:flipper:0.52.1"
         const val soloader = "com.facebook.soloader:soloader:0.9.0"
     }
 }
