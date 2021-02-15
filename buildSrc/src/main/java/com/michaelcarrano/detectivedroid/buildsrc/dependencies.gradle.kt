985cc9450a840f45ffd702c7eecd3814a66870d8
Michael Carrano <carrano.michael@gmail.com>
2021-02-15 10:51:58 -0500

    Disable automatic library update job (#53)

--- buildSrc/src/main/java/com/michaelcarrano/detectivedroid/buildsrc/dependencies.gradle.kt
+++ buildSrc/src/main/java/com/michaelcarrano/detectivedroid/buildsrc/dependencies.gradle.kt
@@ -5,7 +5,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha05"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha06"
 
     const val jacoco = "org.jacoco:org.jacoco.core:0.8.6"
 
