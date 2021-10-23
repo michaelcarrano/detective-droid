b8431d535c87c9238e7762ccefbcec35c698d1b7
Michael Carrano <carrano.michael@gmail.com>
2021-10-23 15:19:08 -0400

    Handle PackageManager returning null values (#57)

--- app/src/main/java/com/michaelcarrano/detectivedroid/data/model/AppEntity.kt
+++ app/src/main/java/com/michaelcarrano/detectivedroid/data/model/AppEntity.kt
@@ -1,8 +1,8 @@
 package com.michaelcarrano.detectivedroid.data.model
 
 data class AppEntity(
-    val name: String,
-    val packageName: String,
-    val versionName: String,
+    val name: String?,
+    val packageName: String?,
+    val versionName: String?,
     val userApp: Boolean
 )
--- app/src/test/java/com/michaelcarrano/detectivedroid/domain/GetAppListUseCaseTest.kt
+++ app/src/test/java/com/michaelcarrano/detectivedroid/domain/GetAppListUseCaseTest.kt
@@ -1,8 +1,10 @@
 package com.michaelcarrano.detectivedroid.domain
 
+import android.content.res.Resources
 import com.michaelcarrano.detectivedroid.data.AppRepository
 import com.michaelcarrano.detectivedroid.data.model.AppEntity
 import com.michaelcarrano.detectivedroid.presentation.model.AppMapper
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
@@ -10,13 +12,18 @@ import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito
 
 class GetAppListUseCaseTest {
+
+    private val resources = mock<Resources>(defaultAnswer = Mockito.RETURNS_DEEP_STUBS) {
+        on { getString(android.R.string.unknownName) } doReturn "Unknown"
+    }
 
     private lateinit var testSubject: GetAppListUseCase
 
     private val repo = mock<AppRepository>()
-    private val mapper = AppMapper()
+    private val mapper = AppMapper(resources)
 
     @Before
     fun setUp() {
@@ -130,6 +137,37 @@ class GetAppListUseCaseTest {
         verify(repo, times(1)).getApplications()
         testObserver.assertNoErrors()
         testObserver.assertValue(mapper.mapToUiModels(allApps))
+        testObserver.dispose()
+    }
+
+    @Test
+    fun `Given apps are installed, when loadApps is called with PackageManager returning null, then return list of apps`() {
+        // GIVEN
+        val showSystemApp = false
+        val systemApps = listOf(
+            AppEntity(null, null, null, false),
+            AppEntity(null, "bar2", "baz2", false),
+            AppEntity("foo3", null, "baz3", false),
+            AppEntity("foo4", "bar4", null, false)
+        )
+
+        val userApps = listOf(
+            AppEntity(null, null, null, true),
+            AppEntity(null, "bar6", "baz6", true),
+            AppEntity("foo7", null, "baz7", true),
+            AppEntity("foo8", "bar8", null, true)
+        )
+
+        val allApps = listOf(userApps, systemApps).flatten()
+        whenever(repo.getApplications()).thenReturn(Single.just(allApps))
+
+        // WHEN
+        val testObserver = testSubject.loadApps(showSystemApp).test()
+
+        // THEN
+        verify(repo, times(1)).getApplications()
+        testObserver.assertNoErrors()
+        testObserver.assertValues(mapper.mapToUiModels(userApps))
         testObserver.dispose()
     }
 }
